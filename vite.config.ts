d29379c11c2d30831f7cd135cbd7d83cd1776bbc
Aj23Mah <ajaymaharjan758@gmail.com>
2025-01-15 22:23:01 +0545

    resolving in the issues in vercel build

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: 'https://github.com/Aj23Mah/food-Ordering', 
-  server: {
-    proxy: {
-      '/assets': {
-        target: 'https://github.com/Aj23Mah/food-Ordering',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/assets/, '/assets')
-      }
-    }
-  }
 })
